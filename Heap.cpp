82f6496bb04816fa751ec7439f66e8c858d50eb1
Alex Kang <Alex@169-231-135-69.wireless.ucsb.edu>
2016-02-06 17:00:02 -0800

    changed heap from template to node

--- Heap.cpp
+++ Heap.cpp
@@ -2,32 +2,32 @@
 #include <iostream>
 
 
-template <class T>
-Heap<T>::Heap(){
+
+Heap::Heap(){
 	numElements = 1; //initialize to one so our calculations are easier. Means that arr[1] is now the min. 
-	ARR_SIZE = 32;
-	arr = new T[ARR_SIZE]; 
+	size = 32;
+	arr = new Node[ARR_SIZE]; 
 }
 
-template <class T>
-int Heap<T>::getParent(const int &child) {
+
+int Heap::getParent(const int &child) {
 	return child/2;
 }
 
-template <class T>
-int Heap<T>::getLeftChild(const int &parent) {
+
+int Heap::getLeftChild(const int &parent) {
 	return 2*parent;
 }
 
-template <class T>
-int Heap<T>::getRightChild(const int &parent) {
+
+int Heap::getRightChild(const int &parent) {
 	return 2*parent + 1;
 }
 
-template <class T>
-void Heap<T>::insert(T const& v){
+
+void Heap::insert(Node const& n){
 	if(numElements < ARR_SIZE){
-		this->arr[numElements] = v;
+		this->arr[numElements] = n;
 		++(this->numElements);
 		//percolate();
 		std::cout<< "success" << std::endl;
@@ -37,25 +37,25 @@ void Heap<T>::insert(T const& v){
 	}
 }
 
-template <class T>
-void Heap<T>::deleteMin(){
+
+void Heap::deleteMin(){
 	this->arr[1] = this->arr[numElements];
 	//percolate();
 	
 }
 
 
-template <class T>
-void Heap<T>::percolateDown(int location){
-	T tmp = this->T[location];
+
+void Heap::percolateDown(int location){
+	Node tmp = this->arr[location];
 	int child = location;
 	for(location; 2*location <= numElements; location = child){
-		child = 2*location;
-		if(child != numElements && this->T[child+1] < this->T[child])
+		child = this->getLeftChild(location);
+		if(child != numElements && this->arr[child+1] < this->arr[child])
 			++child;
-		if(this->T[child] < this->T[location]){
-			this->T[location] = this->T[child];
-			this->T[child] = tmp;
+		if(this->arr[child] < this->arr[location]){
+			this->arr[location] = this->arr[child];
+			this->arr[child] = tmp;
 		}
 		else
 			break;
@@ -64,10 +64,19 @@ void Heap<T>::percolateDown(int location){
 
 }
 
-template <class T> 
-void Heap<T>::buildHeap(T *arr, int size){
 
+void Heap::percolateUp(int location){
+	Node tmp = this->arr[location];
+	int parent = location;
+	for(location; 2/location > 0; location = parent){
+		parent = this->getParent(location);
+	}
 }
+
+/* 
+void Heap::buildHeap(T *arr, int currentSize){
+
+}*/
 
 
 
--- Heap.h
+++ Heap.h
@@ -1,19 +1,20 @@
 #ifndef __Heap_H__
 #define __Heap_H__
 
-template <class T>
+
+#include "Node.h"
 class Heap{
 
 private:
 	
-	int ARR_SIZE;
-	T *arr;
+	int size;
+	Node *arr;
 	int numElements;
 
 public:
 	Heap();
 	~Heap();
-	Heap(T *arr, int currentSize); //build a heap from a given array.
+	//Heap(T *arr, int currentSize); //build a heap from a given array.	
 
 	int getParent(const int &child);
 	int getLeftChild(const int &parent);
@@ -21,13 +22,14 @@ public:
 	
 	//void minHeapify(int *a, int i, int size); //min Heap, so first element in array will be the smallest #
 	
-	void insert(T const&);
+	void insert(Node const&);
 	void print();
 	void deleteMin(); 
 
-	void buildHeap(T *arr, int size); //requires an array input
+	//void buildHeap(T *arr, int size); //requires an array input
 
 	void percolateDown(int location);
+	void percolateUp(int location);
 
 
 
